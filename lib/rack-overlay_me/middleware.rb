5f3862e43251738730f25e20a9116c4dfac2a0fc
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2013-02-01 16:59:43 +1100

    initial implementation

--- lib/rack-overlay_me/middleware.rb
+++ lib/rack-overlay_me/middleware.rb
@@ -6,7 +6,16 @@ module Rack
       end
 
       def call(env)
-        @app.call(env)
+        if env['PATH_INFO'] == '/overlay_me.js'
+          [200, {"Content-Type" => 'text/javascript'}, File.read(File.expand_path('../../../javascripts/overlay_me.js',__FILE__))]
+        else
+          status, headers, body = @app.call(env)
+          new_body = []
+          body.each do |body_part|
+            new_body << body_part.gsub('</body>', "<script type=\"text/javascript\" src=\"/overlay_me.js\"></script></body>")
+          end
+          [status, headers, new_body]
+        end
       end
     end
   end
--- spec/middeware_spec.rb
+++ spec/middeware_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe Rack::OverlayMe::Middleware do
-  it "should return the overlayme javascript on the path '/javascripts/overlay_me.js"
+  it "should return the overlayme javascript on the path '/overlay_me.js" do
+    app = lambda{ |env| [200, {"Content-Type" => 'text/html'}, ["<html><head></head><body><h1>Hello World</h1></body>"]]}
+    middleware = Rack::OverlayMe::Middleware.new(app)
+    status, headers, body = middleware.call("PATH_INFO" => '/overlay_me.js')
+    body.should == File.read(File.expand_path('../../javascripts/overlay_me.js',__FILE__))
+  end
 
-  it "should append the javascript tag for overlayme javascript to the body"
+  it "should append the javascript tag for overlayme javascript to the body" do
+    app = lambda{ |env| [200, {"Content-Type" => 'text/html'}, ["<html><head></head><body><h1>Hello World</h1></body>"]]}
+    middleware = Rack::OverlayMe::Middleware.new(app)
+    status, headers, body = middleware.call({})    
+    body.should == ["<html><head></head><body><h1>Hello World</h1><script type=\"text/javascript\" src=\"/overlay_me.js\"></script></body>"]
+  end
 end
